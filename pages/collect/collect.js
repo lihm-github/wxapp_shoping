2383fd04b2b452033d89c81d913a45f9fdbb305b
李大明 <2324814076@qq.com>
2022-09-20 17:17:21 +0800

    完成项目订单详情、收藏店铺页面功能

--- pages/collect/collect.js
+++ pages/collect/collect.js
@@ -1,66 +1,71 @@
-// pages/collect/collect.js
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad(options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady() {
-
+    collect: [], // 商品收藏数组
+    defaultImage: "../../images/1.jpg", // 商品没有对应图片时显示的默认图片
+    // 需要传递给标签导航Tabs组件的数据
+    tabs: [{
+        id: 0,
+        name: "商品收藏",
+        isActive: true,
+      },
+      {
+        id: 1,
+        name: "品牌收藏",
+        isActive: false,
+      },
+      {
+        id: 2,
+        name: "店铺收藏",
+        isActive: false,
+      },
+      {
+        id: 3,
+        name: "浏览足迹",
+        isActive: false,
+      },
+    ],
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow() {
-
+    // 获取页面跳转携带过来的参数，根据微信提供的api，获取到所有页面的数据项，最后一个元素是当前页面
+    const pages = getCurrentPages()
+    const options = pages[pages.length - 1].options // 获取当前页面
+    // 激活选中对应的 tab 栏
+    this.changeActivedTab(parseInt(options.type) - 1)
+    // 获取缓存中的收藏数据
+    const collect = wx.getStorageSync("collect") || []
+    this.setData({
+      collect,
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide() {
-
+  // 根据页面跳转携带的 type 值激活选中对应的 tab 栏
+  changeActivedTab(type) {
+    let tabs = JSON.parse(JSON.stringify(this.data.tabs)) // 格式化数据
+    // 只有当选选中的tab栏会设为true,选中
+    tabs.forEach((v, i) =>
+      i === type ? (v.isActive = true) : (v.isActive = false)
+    )
+    this.setData({
+      tabs,
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload() {
-
+  // 标题点击事件的处理函数
+  handleTap(e) {
+    // 获取子组件传递过来的数据
+    const {
+      index
+    } = e.detail
+    // 激活选中对应的 tab 栏
+    this.changeActivedTab(index)
   },
 
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh() {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom() {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage() {
-
-  }
 })
--- pages/order/order.js
+++ pages/order/order.js
@@ -1,66 +1,98 @@
-// pages/order/order.js
+import {
+  request
+} from "../../request/index.js"
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad(options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady() {
-
+    orders: [], // 订单信息
+    // 床给Tab栏的数据
+    tabs: [{
+        id: 0,
+        name: "全部",
+        isActive: true,
+      },
+      {
+        id: 1,
+        name: "待付款",
+        isActive: false,
+      },
+      {
+        id: 2,
+        name: "待发货",
+        isActive: false,
+      },
+      {
+        id: 3,
+        name: "退款/退货",
+        isActive: false,
+      },
+    ],
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow() {
-
+    const token = wx.getStorageSync("token") // 订单，要有用户才有订单，获取用户 token
+    // 如果本地没有token，返回假，取反就是真，需要跳到auth页面；如果有token就不执行if，执行往下代码
+    if (!token) {
+      wx.navigateTo({
+        url: "/pages/auth/auth",
+      })
+      return
+    }
+    // 获取小程序的页面栈数组 其中最后一个元素就是为当前页面
+    // 根据页面栈数组获取页面跳转所携带过来的参数
+    const pages = getCurrentPages()
+    const options = pages[pages.length - 1].options
+    // 发送请求获取订单列表；1:全部订单 2:待付款 3:待发货
+    this.getOrders(parseInt(options.type))
+    // 激活选中对应的 tab 栏
+    this.changeActivedTab(parseInt(options.type) - 1)
   },
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide() {
-
+  // 发送请求获取订单列表
+  async getOrders(type) {
+    const res = await request({
+      url: "/my/orders/all",
+      data: {
+        type
+      }
+    })
+    // 获取订单列表数据，更新data中的数据
+    this.setData({
+      orders: res.orders.map(v => {
+        return {
+          ...v,
+          create_time_format: new Date(v.create_time * 1000).toLocaleString(),
+        }
+      })
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload() {
-
+  // 根据页面跳转携带的 type 值激活选中对应的 tab 栏
+  changeActivedTab(type) {
+    let tabs = JSON.parse(JSON.stringify(this.data.tabs))
+    tabs.forEach((v, i) =>
+      i === type ? (v.isActive = true) : (v.isActive = false)
+    )
+    this.setData({
+      tabs,
+    })
   },
 
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh() {
-
+  // 标题点击事件的处理函数
+  handleTap(e) {
+    const {
+      index
+    } = e.detail
+    // 激活选中对应的 tab 栏
+    this.changeActivedTab(index)
+    // 重新发送请求获取对应的订单列表
+    this.getOrders(index + 1)
   },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom() {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage() {
-
-  }
 })
